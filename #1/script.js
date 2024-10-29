c54731e9c1a549800eaa2da5c700dcf0e0be3e5b
s.soltaniant <s.soltaniant@gmail.com>
2024-10-30 01:10:52 +0330

    done first assignment

---
+++ #1/script.js
@@ -0,0 +1,62 @@
+// class QuoteComponent extends HTMLElement {
+
+// 	constructor(...args) {
+//         super(...args);
+//     }
+
+//     static get observedAttributes() {
+//         return ['id', 'author', 'quote'];
+//     }
+//     get author() {
+//         return this.getAttribute('author');
+//     }
+//     get quote() {
+//         return this.getAttribute('quote');
+//     }
+//     attributeChangedCallback(attrName, oldVal, newVal) {
+//         this.render();
+//     }
+//     render(){
+//     	var template = `<div class="quote">`
+// 	template += `<span class="quoted">“${this.quote}”</span>`
+//         template += `<span class="author">- ${this.author}</span>`
+// 	template += `</div>`;
+//         this.innerHTML = template;
+//     }
+// }
+
+
+// window.customElements.define('post', QuoteComponent);
+
+
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const postTags = document.querySelectorAll("Post");
+
+const posts = [];
+postTags.forEach(element => {
+
+    let title = element.getAttribute("title");
+    let content = element.textContent;
+
+    let postContainer = document.createElement("div");
+    let postTitle = document.createElement("h2");
+    let postContent = document.createElement("article");
+    
+    postTitle.textContent = title;
+    postContent.textContent = content;
+
+    postTitle.classList.add("post-title");
+    postContent.classList.add("post-content");
+    postContainer.classList.add("post")
+
+    postContainer.appendChild(postTitle);
+    postContainer.appendChild(postContent);
+
+    container.removeChild(element)
+    container.appendChild(postContainer)
+});
+
+// ideas: labels and category of post, add date for each post + black dark footer as:  https://dribbble.com/shots/24565936-Minimal-blog-Untitled-UI
+// https://animated-fluent-emoji.vercel.app/
+
